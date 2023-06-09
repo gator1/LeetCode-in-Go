a2fa7afc2f7f2307ef3b1168ea76b4080fa48516
Guangsong Xia <garyxia@gmail.com>
2023-06-09 14:26:19 -0700

    improvement

--- Algorithms/0004.median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
+++ Algorithms/0004.median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -1,43 +1,66 @@
 package problem0004
+import "math"
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	nums := combine(nums1, nums2)
-	return medianOf(nums)
-}
-
-func combine(mis, njs []int) []int {
-	lenMis, i := len(mis), 0
-	lenNjs, j := len(njs), 0
-	res := make([]int, lenMis+lenNjs)
-
-	for k := 0; k < lenMis+lenNjs; k++ {
-		if i == lenMis ||
-			(i < lenMis && j < lenNjs && mis[i] > njs[j]) {
-			res[k] = njs[j]
-			j++
-			continue
-		}
-
-		if j == lenNjs ||
-			(i < lenMis && j < lenNjs && mis[i] <= njs[j]) {
-			res[k] = mis[i]
-			i++
-		}
-	}
-
-	return res
-}
-
-func medianOf(nums []int) float64 {
-	l := len(nums)
-
-	if l == 0 {
+	if len(nums1) == 0 && len(nums2) == 0 {
 		panic("切片的长度为0，无法求解中位数。")
 	}
 
-	if l%2 == 0 {
-		return float64(nums[l/2]+nums[l/2-1]) / 2.0
+	if len(nums1) > len(nums2) {
+		return findMedianSortedArrays(nums2, nums1)
 	}
+	
+    left := 0
+	right := len(nums1)
 
-	return float64(nums[l/2])
+    for left <=right {
+		x := left + (right-left)/2
+		y := (len(nums1)+len(nums2)+1) / 2 - x
+	
+		xL := -math.MaxInt32
+		if x > 0 {
+			xL = nums1[x-1]
+		} 
+		xR := math.MaxInt32
+		if x < len(nums1)  {
+			xR = nums1[x]
+		}
+		yL := -math.MaxInt32
+		if y > 0 {
+			yL = nums2[y-1]
+		}  
+		yR := math.MaxInt32
+		if y < len(nums2)  {
+			yR =nums2[y]
+		}
+		if xL <= yR && yL <= xR {
+			if (len(nums1)+len(nums2)) % 2 != 0 {
+				return float64(max(xL, yL))
+			}
+			return float64(max(xL, yL)+min(xR,yR)) / 2.0
+		} else if xR > yL {
+			right = x -1
+		} else {
+			left = x + 1
+		}
+			
+	   
+	}
+	panic("切片的长度为0，无法求解中位数。")
+
 }
